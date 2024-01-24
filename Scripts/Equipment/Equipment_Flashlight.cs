3face0b590fb19a65bed7abad9d251bd902fad74
ArmisDev <113211223+ArmisDev@users.noreply.github.com>
2024-01-24 15:03:45 -0600

    Equipment Commit

---
+++ Scripts/Equipment/Equipment_Flashlight.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(PlayerInput), typeof(AudioSource))]
+public class Equipment_Flashlight : MonoBehaviour
+{
+    private PlayerInput input;
+    private Light flashlight;
+    private InputAction flashlightToggle;
+    private AudioSource audioSource;
+    [SerializeField] private AudioClip flashlightToggleSound;
+    [SerializeField] private bool isFlashlightOn = false;
+
+    private void Awake()
+    {
+        input = GetComponent<PlayerInput>();
+        flashlightToggle = input.actions["Flashlight"];
+        flashlight = GetComponentInChildren<Light>();
+        if(flashlight == null)
+        {
+            Debug.LogError("Please add to the child of flashlight component!!");
+        }
+        if (!isFlashlightOn) flashlight.gameObject.SetActive(false);
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(flashlightToggle.WasPerformedThisFrame())
+        {
+            isFlashlightOn = !isFlashlightOn; // Toggle the flashlight state
+            flashlight.gameObject.SetActive(isFlashlightOn);
+            if (flashlightToggleSound != null)
+            {
+                audioSource.PlayOneShot(flashlightToggleSound);
+            }
+        }
+    }
+}
